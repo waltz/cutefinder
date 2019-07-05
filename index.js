66ef08828004adabdb18a951030d853b821cadef
CJ Bryan <cj@ministryofvelocity.com>
2019-07-05 12:07:02 -0700

    Add Express and configure a tiny web server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Welcome to CuteFinder!')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Welcome to CuteFinder!'))
+
+app.listen(port, () => console.log(`CuteFinder is listening on ${port}`))
